de85cfc74fa37a15a9ebc37c1fd1495cc60f7cf8
dszflaivin <20cs051.flaivin@sjec.ac.in>
2025-03-26 15:13:09 +0530

    updated RegistrationPage

--- src/RegistrationPage.jsx
+++ src/RegistrationPage.jsx
@@ -197,7 +197,7 @@ export default function RegistrationPage() {
             >
               <option value="" disabled>Select your role</option>
               <option value="CUSTOMER">Customer</option>
-              <option value="ADMIN">Admin</option>
+              
             </select>
           </div>
           <button type="submit" className="form-button">Sign Up</button>
